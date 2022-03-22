6023b88fdafad536a9c9734d29c18164256b5ea7
witbunny <witbunny@163.com>
2022-03-22 21:46:46 +0800

    New three files to test MVC.

---
+++ sssmvc/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using sssmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sssmvc.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            RegisterModel model = new RegisterModel
+            {
+                Name = "leo",
+                Password = "1234"
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ sssmvc/Models/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sssmvc.Models
+{
+	public class RegisterModel
+	{
+		public string Name { get; set; }
+		public string Password { get; set; }
+	}
+}
